aeeb436dd49b839bb17a219c650ce97f5f84111e
Mohamed Kaba <muhamad.kaba@gmail.com>
2018-12-19 16:33:49 +0100

    creation de la servlet HomeServlet

--- src/HomeServlet.java
+++ src/HomeServlet.java
@@ -22,7 +22,7 @@ public class HomeServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
+	/**+
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
